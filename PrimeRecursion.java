b1b5447fc9d80fa6cbc872baec8abcb1983f8f27
Aakash <aakashpavar87@gmail.com>
2023-08-05 17:13:47 +0530

    Modified If Else

--- PrimeRecursion.java
+++ PrimeRecursion.java
@@ -4,8 +4,7 @@ public class PrimeRecursion {
     public static void prime(int n, int x) {
         if (n % x == 0) {
             System.out.println(n + " is not prime");
-        }
-        if (x == n) {
+        } else if (x == n) {
             System.out.println(n + " is a prime number.");
         } else{
             prime(n,x+1);
