f22f8b76567d045de9fb8a8ab7abbb7f05af7475
Aakash <aakashpavar87@gmail.com>
2023-08-05 19:27:24 +0530

    Completed code of prime number

--- PrimeRecursion.java
+++ PrimeRecursion.java
@@ -2,20 +2,22 @@ import java.util.*;
 
 public class PrimeRecursion {
     public static void prime(int n, int x) {
-        if (x == n) {
-            System.out.println(n + " is not prime");
-        } else if  (n % x == 0){
-            System.out.println(n + " is a prime number.");
-        } else{
-            prime(n,x+1);
+        if (n == 1 || n == 0) {
+            System.out.println(n + " is not a prime number.");
+        } else if (x == n) {
+            System.out.println(n + " is prime");
+        } else if (n % x == 0) {
+            System.out.println(n + " is not prime number.");
+        } else {
+            prime(n, x + 1);
         }
     }
 
     public static void main(String args[]) {
-        int num,i=2;
+        int num, i = 2;
         Scanner sc = new Scanner(System.in);
         num = sc.nextInt();
-        prime(num, i);   
+        prime(num, i);
         sc.close();
     }
 }
